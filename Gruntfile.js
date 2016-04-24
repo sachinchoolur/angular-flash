cb2d1f2f6a74042a15818e53fb762cf5cab14882
Roope Hakulinen <roope.hakulinen@gmail.com>
2016-04-24 11:08:56 +0300

    Add source map also for the uglified file and also remove double banner from it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
         },
         uglify: {
             options: {
-                banner: '<%= banner %>'
+                sourceMap: true
             },
             dist: {
                 src: distPath + 'angular-flash.js',
